1ae07bd58435e124115ad7880a7c7f6c7f5e0da7
Ramesh A <ramesharpu@gmail.com>
2024-03-03 09:42:52 +0530

    initial commit

---
+++ src/test/java/test/BaseTest.java
@@ -0,0 +1,28 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class BaseTest {
+	
+	public static WebDriver driver;
+	String url = "https://www.fb.com";
+	
+	@BeforeTest
+	public void setup() {
+		//open the browser here
+		driver = new ChromeDriver();//opens the chrome browser
+		
+		//open the url
+		driver.get(url);
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		//close the browser
+		driver.quit();
+	}
+
+}
---
+++ src/test/java/test/BasicValidation.java
@@ -0,0 +1,22 @@
+package test;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class BasicValidation extends BaseTest{
+
+	@Test
+	public void urlValidation() {
+		String expectedResult = "https://www.facebook.com/ramesh";
+		String actualResult = driver.getCurrentUrl();
+		
+		//display
+		Reporter.log("expected result = " + expectedResult);
+		Reporter.log("actual result = " + actualResult);
+		
+		//comparision
+		assertTrue(actualResult.equals(expectedResult), "Mismatch in the url");
+	}
+}
