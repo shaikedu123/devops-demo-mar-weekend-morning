95bba19237080833860aa1daaf95746e245a774f
Ramesh A <ramesharpu@gmail.com>
2024-03-10 08:34:56 +0530

    Update BasicValidation.java

--- src/test/java/test/BasicValidation.java
+++ src/test/java/test/BasicValidation.java
@@ -20,5 +20,6 @@ public class BasicValidation extends BaseTest{
 		assertTrue(actualResult.equals(expectedResult), "Mismatch in the url");
 		//some code changes
 		//cc from b1 branch
+		//demo for stash- dev2 cc
 	}
 }
