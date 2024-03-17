4e159edf84879333f7af8c97e876a6155cee8af6
Ramesh A <ramesharpu@gmail.com>
2024-03-17 07:31:41 +0530

    modified the project with testng.xml and surefire plugin

--- src/test/java/test/BasicValidation.java
+++ src/test/java/test/BasicValidation.java
@@ -9,7 +9,7 @@ public class BasicValidation extends BaseTest{
 
 	@Test
 	public void urlValidation() {
-		String expectedResult = "https://www.facebook.com/ramesh";
+		String expectedResult = "https://www.facebook.com/";
 		String actualResult = driver.getCurrentUrl();
 		
 		//display
